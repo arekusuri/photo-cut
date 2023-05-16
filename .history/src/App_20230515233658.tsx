c4afbbf81308088c983099e2391e1a8ad01e35c2
Alex Li <alex.li@chime.com>
2023-05-15 23:53:18 -0700

    v2

---
+++ .history/src/App_20230515233658.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import ImageCropper from './ImageCrop';
+
+function App() {
+  return (
+    <div className="App">
+       <ImageCropper /> 
+    </div>
+  );
+}
+
+export default App;
